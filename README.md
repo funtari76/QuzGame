472012afe3c6ace879b37b1ca95660fa2aff7360
funtari76 <funtari777@gmail.com>
2022-04-29 09:34:34 +0000

    have a few issus with code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Q Game
+
+This is a grid-based game in JavaScript, HTML and CSS.
+
+# Inbuilt functions
+
+- for loop
+- addEventListener
+- setAttribute
+- getAttribute
+- createElement
+
+## Rules of Gamer
+
+## Credit
+
+freecodecamp.org
---
+++ assets/JS/script.js
@@ -0,0 +1,83 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  // console.log("Started");
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion(); // [1]The very first thing the game should do
+}
+
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answer.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer() {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  nextButton.classList.remove("hide");
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+//**** [2] List of questions****//
+//***Här kan man byta till annat'Vad är?' */
+const questions = [
+  {
+    question: "What is 5 + 5",
+    answer: [
+      { text: "10", correct: true },
+      { text: "55", correct: false },
+    ],
+  },
+];
